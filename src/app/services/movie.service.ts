b3d87c723374181a53e1ab7a5e0bcfd1066f4352
Thor Sem Marreta <eltonpersike@live.com>
2025-01-12 23:35:08 -0300

    documento movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,3 +1,6 @@
+/**
+ * MovieService is an Angular service that provides methods to interact with the movie API.
+ */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,22 +13,48 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Fetches years with multiple winners from the API.
+   * @returns An Observable containing the response from the API.
+   */
   getYearsWithMultipleWinners(): Observable<any> {
     return this.http.get(`${this.apiUrl}?projection=years-with-multiple-winners`);
   }
 
+  /**
+   * Fetches studios with their win count from the API.
+   * @returns An Observable containing the response from the API.
+   */
   getStudiosWithWinCount(): Observable<any> {
     return this.http.get(`${this.apiUrl}?projection=studios-with-win-count`);
   }
 
+  /**
+   * Fetches the maximum and minimum win interval for producers from the API.
+   * @returns An Observable containing the response from the API.
+   */
   getProducersWinInterval(): Observable<any> {
     return this.http.get(`${this.apiUrl}?projection=max-min-win-interval-for-producers`);
   }
 
+  /**
+   * Fetches winners by year from the API.
+   * @param year The year to filter winners by.
+   * @param winner A boolean indicating whether to filter by winners.
+   * @returns An Observable containing the response from the API.
+   */
   getWinnersByYear(year: number, winner: boolean): Observable<any> {
     return this.http.get(`${this.apiUrl}?winner=${winner}&year=${year}`);
   }
 
+  /**
+   * Fetches movies with pagination and optional filters from the API.
+   * @param page The page number for pagination.
+   * @param size The number of items per page.
+   * @param winner A boolean indicating whether to filter by winners.
+   * @param year The year to filter movies by.
+   * @returns An Observable containing the response from the API.
+   */
   getMovies(page: number, size: number, winner: boolean, year: number): Observable<any> {
     let queryParams = [
       `page=${page}`,
